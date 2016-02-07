bab686045dbfa880fd851a8d83b5c8aed9eef442
Kieran Eglin <kieran.eglin@gmail.com>
2016-02-06 16:28:43 -0800

    Pushing to github to get 'repo' field

--- app-server.js
+++ app-server.js
@@ -2,3 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('./public'));
+app.use(express.static('./node_modules/bootstrap/dist'));
+
+app.listen(3000);
+console.log('Server is running on 3000');
